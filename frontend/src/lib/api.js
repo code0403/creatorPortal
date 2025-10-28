8780709b003c20d9a4366bd4eb985663b627d530
Abhishek Singh <sabhishek900@gmail.com>
2025-10-28 11:34:42 +0530

    Fix: render build and api baseURL

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
-  baseURL: import.meta.env.MODE === "development" ? 'http://localhost:5000/api",' : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
   withCredentials: true,
 });
 
